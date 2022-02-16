6458ae1f492d717fda4833f15b8966508b600e41
EC2 Default User <ec2-user@ip-172-31-22-173.ec2.internal>
2022-02-16 10:21:06 +0000

    [Public-top]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
@@ -9,7 +10,11 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
 }
 
-  root to: 'homes#top'
+root to: 'public/homes#top'
+
+
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

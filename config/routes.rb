40da9e1bc29effbb4712e6c6a1443d1795bc0c21
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-02-18 01:45:26 +0000

    [Finish]管理者側商品新規登録画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
 namespace :admin do
   resources :genres, only: [:index, :create, :edit, :update]
+  resources :items, only: [:new, :index, :create, :show, :edit, :update]
 end
 
 root to: 'public/homes#top'

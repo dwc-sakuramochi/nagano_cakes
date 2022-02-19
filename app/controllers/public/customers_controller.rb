29c2e72c96227803de6d979ddc72108548c51899
EC2 Default User <ec2-user@ip-172-31-17-7.ec2.internal>
2022-02-19 06:09:17 +0000

    配送先一覧、編集機能作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -13,22 +13,22 @@ class Public::CustomersController < ApplicationController
     @customer.update(customer_params)
     redirect_to mypage_customers_path
   end
-  
+
   def unsubscribe
     @customer = current_customer
   end
-  
+
   def withdraw
     @customer = current_customer
     @customer.update(withdraw: true)
     reset_session
     redirect_to root_path
   end
-  
+
 private
 
   def customer_params
-    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :post_code, :address, :tel, :withdraw)
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :post_code, :address, :tel, :withdraw, :email)
   end
 
 end

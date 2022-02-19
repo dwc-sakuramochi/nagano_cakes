fa719678f53aac44b9596501d750bc0f5f315c3b
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-02-19 05:30:46 +0000

    [Update]カート内一覧画面（未完成）

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,11 @@
 class Public::CartItemsController < ApplicationController
+  def index
+    # @cart_items = current_customer.cart_items.all
+  end
+  
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
+  end
 end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :item_image
   belongs_to :genre
+  has_many :cart_items, dependent: :destroy
   
   def add_tax_price
     (self.non_taxed_price * 1.1).round

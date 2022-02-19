79e85a2b236199c252825863eab92beed1727d97
EC2 Default User <ec2-user@ip-172-31-22-173.ec2.internal>
2022-02-19 08:40:46 +0000

    public_order_new

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,12 @@
 class Public::OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.save
+    redirect_to order_path
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+  belongs_to :customer
+  has_many :order_detail
+  enum payment_method: { クレジットカード: 0, 銀行振込: 1 }
 end

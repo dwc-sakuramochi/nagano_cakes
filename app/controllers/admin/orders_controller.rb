d4e6b238ceff392e6b6ea2630809c18089ea1773
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-02-22 05:13:37 +0000

    [Finish]管理者注文履歴詳細画面の機能作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,18 @@
 class Admin::OrdersController < ApplicationController
   def show
-    @order = Order.find(params[:order_id])
-    @order_detail = OrderDetail.find(params[:order_detail_id])
+    @order = current_customer.orders.find(params[:id])
+    @order_details = @order.order_details
+    @sum = 0
+  end
+  
+  def update
+    order = current_customer.orders.find(params[:id])
+    order.update(order_params)
+    redirect_to request.referer
+  end
+  
+private 
+  def order_params
+    params.require(:order).permit(:order_status)
   end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,4 +3,8 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   
   enum product_status: { 製作不可: 0, 製作待ち: 1, 製作中: 2, 製作完了: 3}
+  
+  def subtotal
+    item.add_tax_price * amount
+  end
 end

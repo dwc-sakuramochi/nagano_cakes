b1bbf1e268f71043b5e3798ac43ae57f730ce6cb
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-02-22 16:18:10 +0000

    [Update]管理者バリデーション、製作・注文ステータスの更新（仮）

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,8 +1,28 @@
 class Admin::OrderDetailsController < ApplicationController
+  before_action :authenticate_admin
   def update
-    @order = current_customer.orders.find(params[:order_id])
-    order_details = @order.order_details.find(params[:id])
+    order = Order.find(params[:order_id])
+    order_details = order.order_details.find(params[:id])
     order_details.update(order_detail_params)
+
+    order.update(order_status: "製作中")if order_details.product_status == "製作中"
+
+    order.order_details.each do |order_detail|
+      if order_detail.product_status == "製作完了"
+        order.update(order_status: "発送準備中")
+      end
+    end
+
+    order.order_details.each do |order_detail|
+      if order_detail.product_status != "製作完了"
+        order.update(order_status: "製作中")
+      end
+    end
+
+    # if order_details.where(product_status: "製作完了").count == order_details.count
+    #   order.update(order_status: "発送準備中")
+    # end
+    
     redirect_to request.referer
   end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :post_code, :address, :tel])
   end
+
+  def authenticate_admin
+    if current_admin==nil
+      flash[:notice]="ログインが必要です"
+      redirect_to root_path
+    end
+  end
 end

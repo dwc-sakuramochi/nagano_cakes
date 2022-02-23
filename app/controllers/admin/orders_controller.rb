371f61ec5ac2c7e4647db525d3dd951a278d01cd
EC2 Default User <ec2-user@ip-172-31-36-77.ec2.internal>
2022-02-23 02:01:47 +0000

    [Fix]トップページのレイアウト

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,17 +1,16 @@
 class Admin::OrdersController < ApplicationController
   def show
-    @order = current_customer.orders.find(params[:id])
+    @order = Order.find(params[:id])
     @order_details = @order.order_details
-    @sum = 0
   end
-  
+
   def update
     order = current_customer.orders.find(params[:id])
     order.update(order_params)
     redirect_to request.referer
   end
-  
-private 
+
+private
   def order_params
     params.require(:order).permit(:order_status)
   end

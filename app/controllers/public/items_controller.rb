88e2ea25f6b8f286e71b759f128de558ec56117b
EC2 Default User <ec2-user@ip-172-31-17-7.ec2.internal>
2022-02-23 02:32:45 +0000

    ヘッダー完成、販売中の商品のみ表示されるように修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,7 @@
 class Public::ItemsController < ApplicationController
 
   def index
-    @items = Item.page(params[:page]).per(8)
+    @items = Item.where(sale_status: "true").page(params[:page]).per(8)
   end
 
   def show

2f6b8fef71c43a1f9e9507e2182ad4cff1181ff0
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-02-23 01:44:30 +0000

    [Fix]管理者注文履歴詳細画面の修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,27 +2,17 @@ class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin
   def update
     order = Order.find(params[:order_id])
-    order_details = order.order_details.find(params[:id])
-    order_details.update(order_detail_params)
+    order_details = order.order_details
+    order_detail = OrderDetail.find(params[:id])
+    order_detail.update(order_detail_params)
 
-    order.update(order_status: "製作中")if order_details.product_status == "製作中"
 
-    order.order_details.each do |order_detail|
-      if order_detail.product_status == "製作完了"
-        order.update(order_status: "発送準備中")
-      end
+    if order_details.where(product_status: "製作完了").count == order_details.count
+      order.update(order_status: "発送準備中")
+    elsif order_details.where(product_status: "製作中").count >= 1
+      order.update(order_status: "製作中")
     end
 
-    order.order_details.each do |order_detail|
-      if order_detail.product_status != "製作完了"
-        order.update(order_status: "製作中")
-      end
-    end
-
-    # if order_details.where(product_status: "製作完了").count == order_details.count
-    #   order.update(order_status: "発送準備中")
-    # end
-    
     redirect_to request.referer
   end
 

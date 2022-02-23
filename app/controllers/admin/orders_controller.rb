70fccd663d06f266e0ac5cd5fcf6489eff84ecd5
EC2 Default User <ec2-user@ip-172-31-36-77.ec2.internal>
2022-02-23 02:08:00 +0000

    [Fix]orders_controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,6 +3,7 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
+    @sum = 0
   end
 
   def update

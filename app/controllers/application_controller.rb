5c603e59d476731ed1873144635e7efb04f7496e
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-02-24 13:45:39 +0900

    修正
    誤って削除してしまったコードを元に戻しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,18 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :post_code, :address, :tel])
   end
   
+  def authenticate_admin
+    if current_admin==nil
+      flash[:notice]="ログインが必要です"
+      redirect_to root_path
+    end
+  end
   
+  def autheniticate_customer
+    if current_customer==nil
+      flash[:notice]="ログインが必要です"
+      redirect_to root_path
+    end
+  end
+   
 end

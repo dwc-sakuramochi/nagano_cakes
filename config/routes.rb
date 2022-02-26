ec3138fefe61c740618173dd56faab31658319e6
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-02-26 02:22:45 +0000

    [Add]ジャンル検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'genres/index'
+  end
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
@@ -9,47 +12,44 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
 }
 
-namespace :admin do
-  get '/' => 'homes#top'
-  resources :genres, only: [:index, :create, :edit, :update]
-  resources :items, only: [:new, :index, :create, :show, :edit, :update]
-  resources :orders, only: [:show, :update] do
-    resources :order_details, only: [:update]
-  end
-  resources :customers, only: [:index, :show, :edit, :update]
-end
-scope module: :public do
- root to: 'homes#top'
- get 'about' => 'homes#about', as: 'about'
-
-
-  resources :items, only: [:index,:show]
-  resources :customers, only: [:edit,:update] do
-    collection do
-      get :mypage
-      get :unsubscribe
-      patch :withdraw
+  namespace :admin do
+    get '/' => 'homes#top'
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:new, :index, :create, :show, :edit, :update]
+    resources :orders, only: [:show, :update] do
+      resources :order_details, only: [:update]
     end
+    resources :customers, only: [:index, :show, :edit, :update]
   end
-
-  resources :cart_items, only: [:index,:update,:destroy,:create] do
-    collection do
-      delete :destroy_all
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about', as: 'about'
+    
+    
+    resources :items, only: [:index,:show]
+    resources :customers, only: [:edit,:update] do
+      collection do
+        get :mypage
+        get :unsubscribe
+        patch :withdraw
+      end
     end
-  end
-
-  resources :orders, only: [:index,:show,:new,:create] do
-    collection do
-      post :confirm
-      get :complete
+    
+    resources :cart_items, only: [:index,:update,:destroy,:create] do
+      collection do
+        delete :destroy_all
+      end
     end
+    
+    resources :orders, only: [:index,:show,:new,:create] do
+      collection do
+        post :confirm
+        get :complete
+      end
+    end
+    
+    resources :shipping_addresses, only: [:index,:create,:destroy,:edit,:update]
+    resources :genres, only: [:show]
   end
-
-  resources :shipping_addresses, only: [:index,:create,:destroy,:edit,:update]
-
-
-
-end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

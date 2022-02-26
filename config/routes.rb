aa4062ee232fb91d0f8524abe74786e53ac6f331
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-02-26 11:29:35 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-
-  namespace :public do
-    get 'genres/index'
-  end
+  
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
